0b3feb02de541d73e5100a16ef98771462891afa
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-01-01 17:13:07 -0300

    Atualizando o exercicio de Arquivos

--- src/exercise/Problem.java
+++ src/exercise/Problem.java
@@ -6,32 +6,53 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
 
 public class Problem {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-		String strPath = "C:\\Programacao";
-		@SuppressWarnings("unused")
-		boolean pi = new File(strPath + "\\out").mkdir();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(strPath + "\\in.txt"));
-				BufferedWriter bw = new BufferedWriter(new FileWriter(strPath + "\\out\\summary.txt"))) {
-
+		List<Product> list = new ArrayList<>();
+		
+		System.out.print("Enter a source path: ");
+		File sourcePath = new File(sc.nextLine());
+		
+		boolean success = new File(sourcePath.getParent()+"\\out").mkdir();
+		String targetPath = sourcePath.getParent()+"\\out\\summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(sourcePath));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(targetPath))){
+			
 			String line = br.readLine();
-			while (line != null) {
-				String[] parts = line.split(",");
-				double total = Double.parseDouble(parts[1]) * Integer.parseInt(parts[2]);
-				bw.write(parts[0] + "," + String.format("%.2f", total));
-				bw.newLine();
+			
+			while(line != null) {
+				String[] values = line.split(",");
+				String name = values[0];
+				double price = Double.parseDouble(values[1]);
+				int quantity = Integer.parseInt(values[2]);
+				
+				list.add(new Product(name,price,quantity));				
+				
 				line = br.readLine();
 			}
-
+			
+			for(Product p : list) {
+				bw.write(p.toString());
+				bw.newLine();			
+			}
+			
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Error: "+e.getMessage());
+		} finally {
+			sc.close();
 		}
 
 	}
